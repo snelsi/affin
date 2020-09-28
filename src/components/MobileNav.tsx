6a8a4d125862ca7a7f82e8835eba880892e4e3d1
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-09-28 19:39:44 +0300

    Fix svg styles case

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -37,9 +37,9 @@ const SidebarIcon = () => (
     <path
       d="M19 3H5a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2V5a2 2 0 00-2-2zM9 3v18"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
@@ -49,9 +49,9 @@ const SearchIcon = () => (
     <path
       d="M11 19a8 8 0 100-16 8 8 0 000 16zM21 21l-4.35-4.35"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
@@ -60,16 +60,16 @@ const BookIcon = () => (
     <path
       d="M4 19.5A2.5 2.5 0 016.5 17H20"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M6.5 2H20v20H6.5A2.5 2.5 0 014 19.5v-15A2.5 2.5 0 016.5 2v0z"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
@@ -78,20 +78,20 @@ const BookmarksIcon = () => (
     <path
       d="M19.07 21l-7-5-7 5V5a2 2 0 012-2h10a2 2 0 012 2v16z"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
 
 const ProfileIcon = () => (
   <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 24">
-    <g clip-path="url(#clip0)" stroke="currentColor" stroke-width="2">
+    <g clipPath="url(#clip0)" stroke="currentColor" strokeWidth="2">
       <path
         d="M19.2 20.5V20a3.5 3.5 0 00-3.5-3.5h-7A3.5 3.5 0 005.2 20v.5M12.2 13a3.5 3.5 0 100-7 3.5 3.5 0 000 7z"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <circle cx="12.2" cy="12" r="11" />
     </g>
