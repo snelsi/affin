c1704abca56474bf35dec173081e68376dba9b59
Roman Zhuravlov <snelsithepirate@gmail.com>
2021-05-10 20:24:42 +0300

    Update next-18n setup

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "uk", "ru"],
+    localePath: path.resolve("./public/static/locales"),
   },
 };
