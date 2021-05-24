7fa0c539e4ffcf7071188a673469c9e21bcdd78d
Roman Zhuravlov <snelsithepirate@gmail.com>
2021-05-24 23:52:06 +0300

    Add id to gradient canvas

--- components/GradientBG/GradientBG.tsx
+++ components/GradientBG/GradientBG.tsx
@@ -5,7 +5,7 @@ import Wave from "./Wave";
 interface GradientBGProps {}
 const GradientBG: React.FC<GradientBGProps> = () => (
   <div className="bg">
-    <Canvas camera={{ position: [0, 0, 5] }}>
+    <Canvas camera={{ position: [0, 0, 5] }} id="gradient">
       <ambientLight />
       <Wave />
     </Canvas>
